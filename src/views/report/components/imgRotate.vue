e3f499c97f51e6a7aeb5b88793eb1135676847d9
daiguda <daisydai@crop.blackfish.cn>
2018-02-13 14:35:34 +0800

    报告修改

---
+++ src/views/report/components/imgRotate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="img-rotote">
+    <div class="btn-top">
+      <span>取消</span>
+      <span>保存</span>
+    </div>
+    <div class="container">
+      <img src="" alt="">
+    </div>
+    <div class="btn-bottom">
+      旋转
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
