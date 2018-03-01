8443f0e7b56e487411ea844764572c07cb28ea3c
daiguda <daisydai@crop.blackfish.cn>
2018-03-01 23:09:31 +0800

    删除 vux

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -65,9 +65,11 @@ var originalConfig = {
   }
 }
 
-const vuxLoader = require('vux-loader')
-const webpackConfig = originalConfig // 原来的 module.exports 代码赋值给变量 webpackConfig
+module.exports = originalConfig
 
-module.exports = vuxLoader.merge(webpackConfig, {
-  plugins: ['vux-ui']
-})
+// const vuxLoader = require('vux-loader')
+// const webpackConfig = originalConfig // 原来的 module.exports 代码赋值给变量 webpackConfig
+
+// module.exports = vuxLoader.merge(webpackConfig, {
+//   plugins: ['vux-ui']
+// })
